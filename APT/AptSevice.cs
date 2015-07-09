7a335689a52a5f9b43a18c5a05073e137e196581
Anderson Silva <anderson.rodrigo@gmail.com>
2015-07-09 08:41:48 -0300

    Carga Inicial
    Projeto inicial

---
+++ APT/AptSevice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APT
+{
+    public class AptSevice: IAptService
+    {
+        public string SolicitarBilhetePortabilidade(ModeloCanonico.Custumer custumer, ModeloCanonico.Acount acount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
